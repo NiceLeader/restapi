b6bc3ddaf4a9c5f11b7254e49daba4049205ac34
NiceLeader <maciek2002.10@wp.pl>
2022-10-16 00:07:22 +0200

    .

---
+++ src/main/java/com/task/restapi/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.task.restapi.controller;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+
+public class ArticleController {
+
+}
--- src/main/java/com/task/restapi/model/ArticleRepository.java
+++ src/main/java/com/task/restapi/model/ArticleRepository.java
@@ -1,11 +1,21 @@
 package com.task.restapi.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Override
+    void deleteById(Long aLong);
 
+    @Override
+    void delete(Article entity);
+@RestResource(path = "by-title-and-content", rel = "by-title-and-content")
+    List<Article> findArticlesByTitleContainingAndContentContaining(@Param("title")String title, String content);
 }
