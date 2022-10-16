5eee2897b78b2464feb8c332f264f0437281f09d
NiceLeader <maciek2002.10@wp.pl>
2022-10-16 18:16:26 +0200

    Final commit(visual) , read me etc next

--- src/main/java/com/task/restapi/controller/ArticleController.java
+++ src/main/java/com/task/restapi/controller/ArticleController.java
@@ -3,15 +3,7 @@ package com.task.restapi.controller;
 
 import com.task.restapi.Service.ArticleService;
 import com.task.restapi.model.Article;
-import org.apache.coyote.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
